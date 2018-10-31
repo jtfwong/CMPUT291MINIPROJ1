0d2c4f1c26f27662ca3bc1f70185f964da20c39a
William Wong <williamwong@Williams-MacBook-Air.local>
2018-10-31 13:20:46 -0600

    Updated login

--- hehe.py
+++ hehe.py
@@ -5,6 +5,9 @@ class JavinDrive():
     def __init__(self):
         #Stuff to initialize
         self.login = False
+        self.user = None
+        self.name = None
+
         self.conn = sqlite3.connect('./JavinDrive.db')
         self.cursor = self.conn.cursor()
         self.cursor.execute('''
@@ -15,6 +18,7 @@ class JavinDrive():
         pwd TEXT,
         PRIMARY KEY (email))''')
         self.conn.commit()
+
     def statement(self):
         print(self.hi)
 
@@ -24,11 +28,30 @@ class JavinDrive():
         '''Hello, Welcome to JavinDrive.\nFor assistance please type help into the command''')
         while not Exit:
             user_input = input('JavinDrive: ')
+            if user_input == 'help':
+                print('help,quit,login,register,logout')
             if user_input == 'quit':
                 Exit = True
             if user_input == 'login':
-                login.logScreen(self.conn,self.cursor)
-
+                if self.user != None:
+                    print(self.user + ' is logged on already.')
+                else:
+                    try:
+                        self.user,self.name = login.logScreen(self.conn,self.cursor)
+                    except:
+                        pass
+            if user_input == 'register':
+                if self.user == None:
+                    try:
+                        self.user,self.name = login.register(self.conn,self.cursor)
+                    except:
+                        pass
+                else:
+                    print(self.user + ' is logged on already.')
+            if user_input == 'logout':
+                self.user = None
+                self.name = None
+                print('Logged out')
 if __name__ == "__main__":
     app = JavinDrive()
     app.app_loop()
--- login.py
+++ login.py
@@ -1,6 +1,8 @@
+import sqlite3
+import getpass
 def logScreen(conn,cursor):
     username = input('Username: ')
-    password = input('Password: ')
+    password = getpass.getpass('Password: ')
     cursor.execute('''
     SELECT *
     FROM Members
@@ -8,4 +10,25 @@ def logScreen(conn,cursor):
     AND Members.pwd = ?;''',
     (username,password))
     result = cursor.fetchone()
-    print(result)
+    if result == None:
+        print('Username and Password not found')
+        return
+    if result[3] == password:
+        print('Welcome ' + result[1])
+        return result[0],result[1]
+
+def register(conn,cursor):
+    username = input('Username: ')
+    name = input('Name: ')
+    phone = input('Phone: ')
+    pwd = input('Password: ')
+    try:
+        cursor.execute('''
+        INSERT INTO Members VALUES
+        (?,?,?,?);''',
+        (username,name,phone,pwd))
+        return username,name
+    except sqlite3.IntegrityError:
+        print('Email already registered')
+    conn.commit()
+    return
